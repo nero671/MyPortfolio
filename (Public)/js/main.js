96a6a665d66663f07bab4b638723cb5870845a41
Nero <s.kachalko@imedia.by>
2024-03-09 15:13:46 +0300

    fixes

--- (Public)/js/main.js
+++ (Public)/js/main.js
@@ -8,9 +8,10 @@ const modal = document.querySelector('.modal');
 const modalClose = document.querySelector('.modal__close');
 const bestWorks = document.querySelector('#best-works .best-works');
 const webWorks = document.querySelector('#sites .web-works');
-const reactWorks = document.querySelector('#react-works .react-works');
+const reactWorks = document.querySelector('#react .react-works');
 const jsWorks = document.querySelector('#js-services .js-works');
 const gamesWorks = document.querySelector('#games-services .games-works');
+const bw = document.body.clientWidth;
 
 
 
@@ -57,7 +58,7 @@ document.addEventListener('keydown', closeModal);
 document.addEventListener('click', closeModal);
 
 
-arrow.addEventListener('click', toContent);
+// arrow.addEventListener('click', toContent);
 
 conectionLogoIcon.addEventListener('click', openModal);
 modalClose.addEventListener('click', closeModal);
@@ -66,284 +67,167 @@ menuModal.addEventListener('click', changeArrow);
 
 
 ////animations
-// Init Setups
-gsap.set(["#bubbles rect", "#bubbles circle", "#bubbles path", "#bubbles2 g"], {
-  transformOrigin: "50% 50%"
-});
 
-// Init Timeline
-const tl = gsap.timeline({
-  paused: false,
-  onComplete: () => {
-    gsap.to(".bubble", {
-      yPercent: -200,
-      scrollTrigger: {
-        trigger: 'svg',
-        scrub: 2,
-        start: 'top top',
-        end: 'bottom top',
-      }
+if (bw > 1400) {
+    gsap.set(["#bubbles rect", "#bubbles circle", "#bubbles path", "#bubbles2 g, bubble"], {
+        transformOrigin: "50% 50%"
     });
 
-    ScrollTrigger.refresh()
-  }
-});
-
-tl.from(
-    ".circle_g",
-    {
-      duration: 2.75,
-      y: 150,
-      ease: "elastic.out(.25, 0.1)",
-      stagger: {
-        from: "random",
-        grid: [1, 4],
-        amount: 1,
-        ease: "power2"
-      }
-    },
-    0
-).from(
-    ".bubble",
-    {
-      duration: 1.5,
-      y: 150,
-      ease: "elastic.out(.1, 0.1)",
-      stagger: {
-        from: "random",
-        grid: [5, 15],
-        amount: 1.5
-      }
-    },
-    0
-).from(
-    ".main-awatar",
-    {
-        duration: 2.6,
-        y: 1050,
-        ease: "elastic.out(.1, 0.1)",
-    },
-    1
-);
-
-// const wrapper = document.querySelector('.wrapper')
-// gsap.registerPlugin(ScrollTrigger);
-// gsap.defaults({ease: "none", duration: 10});
-// let tl3 = gsap.timeline();
-//
-// tl3.to('.panel', {
-//     scale: 0.75,
-// }, 0).
-// from('.panel_animation2', {
-//     opacity: 1,
-//     y: -100,
-// }, 0).
-// to('.panel_animation2', {
-//         opacity: 1
-// }, 0)
-//
-// ScrollTrigger.create({
-//     animation: tl3,
-//     trigger: '.wrapper',
-//     start: "top top",
-//     end: () => wrapper.offsetWidth / 2,
-//     scrub: true,
-//     pin: true,
-//     anticipatePin: 1,
-//     pinType: "fixed",
-// })
-let panels = gsap.utils.toArray(".panel");
-let panelAnimation2 = gsap.utils.toArray(".panel_animation2");
-
-// let triggers = panels.map((panel, i) => {
-//   let tween = gsap.to(panel, {
-//     scale: 0.75, opacity: 1,
-//     scrollTrigger: {
-//       trigger: panel,
-//       start: "bottom bottom",
-//       scrub: 1,
-//       pin: true,
-//       pinSpacing: false
-//     },
-//   });
-// });
-
-gsap.to('.panel', {
-    scrollTrigger: {
-        trigger: '.panel',
-        start: "bottom bottom",
-        scrub: 1,
-        pin: true,
-        pinSpacing: false,
-    },
-    scale: 0.75,
-})
-
-
-// gsap.to('#section1', {
-//     scrollTrigger: {
-//         trigger: '.section-container',
-//         start: 'center 150px',
-//         end: 'center 100px',
-//         pin: true,
-//         anticipatePin: 1,
-//         scrub: 1,
-//         markers: true,
-//     },
-//     opacity: 0,
-//     ease: 'ease',
-//
-//
-// });
-//
-// gsap.set('#section2', { height: '100vh' });
-
-
-// gsap.to('#section2', {
-//     scrollTrigger: {
-//         trigger: '#section2',
-//         start: "top top",
-//         endTrigger: '#section3',
-//         end: "top top",
-//         pin: true,
-//         anticipatePin: 1,
-//         scrub: 1,
-//         markers: true,
-//     },
-//     opacity: 0,
-//     ease: "ease",
-// })
-// gsap.to('#section2', {
-//     scrollTrigger: {
-//         trigger: '#section2',
-//         start: "50px 50px",
-//         end: "+=500",
-//         pin: true,
-//         anticipatePin: 1,
-//         scrub: 1,
-//     },
-//     opacity: 0,
-//     ease: "ease",
-//     y: 100,
-//     duration: 1,
-// })
-
-/////////////////FOR TEST//////////////////
-
-// const tl4 = gsap.timeline();
-//
-// tl4.fromTo('#section1', {opacity: 1}, {y: 0});
-// tl4.fromTo('#section2', {opacity: 0}, {y:0});
-//
-//
-// const sectionWrapper2 = document.querySelector('.panel_animation2__wrapper');
-//
-// ScrollTrigger.create({
-//     animation: tl4,
-//     trigger: '.panel_animation2__wrapper',
-//     start: 'top top',
-//     end: () => sectionWrapper2.offsetWidth / 2,
-//     scrub: true,
-//     pin: true,
-// });
-
-
-panelAnimation2.forEach((section, index) => {
-    const color = section.querySelector(".panel_animation__color");
-
+// Init Timeline
     const tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: section,
-            pin: true,
-            anticipatePin: 1,
-            scrub: 1,
-            start: "50px 50px",
-            end: "+=500",
-            markers: true,
+        paused: false,
+        onComplete: () => {
+            gsap.to(".bubble", {
+                yPercent: -200,
+                scrollTrigger: {
+                    trigger: 'svg',
+                    scrub: 2,
+                    start: 'top top',
+                    end: 'bottom top',
+                }
+            });
+
+            ScrollTrigger.refresh()
         }
     });
 
-    if (index > 0) {
-        tl.from(color, { opacity: 0, ease: "ease", duration: 0.1 });
-    }
+    tl.from(
+        ".circle_g",
+        {
+            duration: 2.75,
+            y: 150,
+            ease: "elastic.out(.25, 0.1)",
+            stagger: {
+                from: "random",
+                grid: [1, 4],
+                amount: 1,
+                ease: "power2"
+            }
+        },
+        0
+    ).from(
+        ".bubble",
+        {
+            duration: 1.5,
+            y: 150,
+            ease: "elastic.out(.1, 0.1)",
+            stagger: {
+                from: "random",
+                grid: [5, 15],
+                amount: 1.5
+            }
+        },
+        0
+    ).from(
+        ".main-awatar",
+        {
+            duration: 2.6,
+            y: 1050,
+            ease: "elastic.out(.1, 0.1)",
+        },
+        1
+    ).to(
+        '.round', {
+            duration: 2.6,
+            css: {
+                left: "-200px"
+            },
+            ease: "elastic.out(.1, 0.1)",
+        }, 4
+    ).to(
+        '.round2', {
+            duration: 2.6,
+            css: {
+                right: "-200px",
+                left: 'initial'
+            },
+            ease: "elastic.out(.1, 0.1)",
+        }, 4
+    );
 
-    if (index < panelAnimation2.length - 1) {
-        tl.to(color, { opacity: 0, ease: "ease", duration: 0.1 });
-    }
-
-
-});
-
-
-const tl2 = gsap.timeline();
-
-tl2.fromTo('#best-works', {x: '-100%', y: '+100%  '}, {y: 0});
-tl2.fromTo('#sites', {x: '-100%'}, {x: '-200%'});
-tl2.fromTo('#react', {x: '-400%'}, {x: '-300%'});
-tl2.fromTo('#js-services', {x: '-200%'}, {x: '-400%'});
-tl2.fromTo('#games', {x: '-300%'}, {x: '-500%'});
-
-const sectionWrapper = document.querySelector('.section-wrapper');
-
-ScrollTrigger.create({
-    animation: tl2,
-    trigger: '.section-wrapper',
-    start: 'top top',
-    end: () => sectionWrapper.offsetWidth / 2,
-    scrub: true,
-    pin: true,
-});
-
-// ScrollTrigger.defaults({
-//     scroller: window,
-//     throttle: 1 // Используйте подходящее значение throttle для вашего случая.
-// });
-
-
-
-const previewTitle = document.querySelector('.preview-title');
-const previewSubtitle = document.querySelector('.preview-subtitle');
-
-const splitText = (el) => {
-    el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
-        return `<div class="word">` +
-            m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
-            `</div>`;
+    gsap.utils.toArray('.panel').forEach((panel) => {
+        ScrollTrigger.create({
+            trigger: panel,
+            start: "top top",
+            scrub: 1,
+            pin: true,
+            pinSpacing: false,
+        });
     });
-    return el;
-};
 
-const split = splitText(previewTitle);
-const splitSub = splitText(previewSubtitle);
+    const tl2 = gsap.timeline();
+    tl2.fromTo('#best-works', {x: 0, y: 0}, {y: 0});
+    tl2.fromTo('#sites', {x: '-100%', y: '-500%'}, {y: 0, ease: Power4.easeOut});
+    tl2.fromTo('#react', {x: '-100%'}, {x: '-200%'});
+    tl2.fromTo('#js-services', {x: '-400%'}, {x: '-300%'});
+    tl2.fromTo('#games', {x: '-100%', y: '-100%'}, {y: '0%'});
 
-function random(min, max){
-    return (Math.random() * (max - min)) + min;
-}
+    const sectionWrapper = document.querySelector('.section-wrapper');
 
-const animateLetter = (el, idx, delay) => {
-    TweenMax.from(el, 1.5, {
+    ScrollTrigger.create({
+        animation: tl2,
+        trigger: '.section-wrapper',
+        start: 'top top',
+        end: () => sectionWrapper.offsetWidth / 2,
+        scrub: true,
+        pin: true,
+    });
+
+    ScrollTrigger.defaults({
+        scroller: window,
+        throttle: 1
+    });
+
+    const previewTitle = document.querySelector('.preview-title');
+    const previewSubtitle = document.querySelector('.preview-subtitle');
+
+    const splitText = (el) => {
+        el.innerHTML = el.textContent.replace(/(\S*)/g, m => {
+            return `<div class="word">` +
+                m.replace(/(-|#|@)?\S(-|#|@)?/g, "<div class='letter'>$&</div>") +
+                `</div>`;
+        });
+        return el;
+    };
+
+    const split = splitText(previewTitle);
+    const splitSub = splitText(previewSubtitle);
+
+    function random(min, max){
+        return (Math.random() * (max - min)) + min;
+    }
+
+    const animateLetter = (el, idx, delay) => {
+        TweenMax.from(el, 1.5, {
+            opacity: 0,
+            scale: 0.1,
+            x: random(-500, 500),
+            y: random(-500, 500),
+            z: random(-500, 500),
+            delay: idx * 0.02,
+            repeat: 0
+        }, delay)
+    }
+
+    Array.from(splitSub.querySelectorAll('.letter')).forEach((el, idx) => {
+        animateLetter(el, idx, 3.4)
+    })
+
+    Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
+        animateLetter(el, idx, 2.8);
+    })
+
+    TweenLite.from('.blob-btn', {
         opacity: 0,
-        scale: 0.1,
-        x: random(-500, 500),
-        y: random(-500, 500),
-        z: random(-500, 500),
-        delay: idx * 0.02,
-        repeat: 0
-    }, delay)
+        scale: 0.25
+    }, 5.2);
+
+    TweenLite.from('.bubble-link', {
+        opacity: 0,
+        scale: 0.25
+    }, 5.5);
 }
 
-Array.from(splitSub.querySelectorAll('.letter')).forEach((el, idx) => {
-    animateLetter(el, idx, 3.4)
-})
-
-Array.from(split.querySelectorAll('.letter')).forEach((el, idx) => {
-    animateLetter(el, idx, 2.8);
-})
-
-TweenLite.from('.blob-btn', {
-    opacity: 0,
-    scale: 0.25
-}, 5.2);
 
 const swiperInit = (selector) => {
     new Swiper(selector, {
@@ -361,26 +245,97 @@ const swiperInit = (selector) => {
         breakpoints: {
             320: {
                 spaceBetween: 10,
-                centeredSlides: true,
-                slidesPerView: 'auto',
+                slidesPerView: 1.5,
             },
-            501: {
+            490: {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+            700: {
                 slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            1400: {
+                slidesPerView: 4,
                 spaceBetween: 30,
             },
         },
     });
-
-    console.log(selector.nextElementSibling)
 }
+
+const scrollToNextSection = () => {
+    const nextSection = document.getElementById('section1');
+    const offset = 70;
+    const targetY = nextSection.offsetTop - offset;
+
+    window.scrollTo({
+        top: targetY,
+        behavior: 'smooth',
+    });
+};
+// const previewLink = document.querySelector('.preview-link');
+// previewLink.addEventListener('click', () => document.body.classList.remove('no-scroll'))
+// previewLink.addEventListener('click', scrollToNextSection);
 
 swiperInit(bestWorks);
 swiperInit(webWorks);
-// swiperInit(reactWorks);
+if (bw < 490) {
+    swiperInit(reactWorks);
+}
+
 swiperInit(jsWorks);
 // swiperInit(gamesWorks);
 
 
+const togglePopupWindows = () => {
+    document.addEventListener('click', ({ target }) => {
+        if (target.closest('[data-type]')) {
+            const popup = document.querySelector(
+                `[data-popup="${target.dataset.type}"]`
+            );
+
+            console.log(popup)
+
+            if (document.querySelector('._is-open')) {
+                document.querySelectorAll('._is-open').forEach((modal) => {
+                    modal.classList.remove('_is-open');
+                });
+            }
+
+            popup.classList.add('_is-open');
+        }
+
+    });
+};
+
+togglePopupWindows();
+
+const skillsItemWrapper = document.querySelectorAll('.skills__item-wrapper');
+
+skillsItemWrapper.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        if (e.target.classList.contains('skills__item-wrapper')) {
+            item.classList.toggle('shattered');
+
+
+            setTimeout(() => {
+                const skillsItem = item.querySelector('.skills__item');
+                if (item.classList.contains('shattered')) {
+                    skillsItem.style.cssText = `z-index: -1`;
+                } else {
+                    skillsItem.style.cssText = `z-index: 1;`;
+                    skillsItem.classList.toggle('reverse-shatter-animation');
+
+                    setTimeout(() => {
+                        if (skillsItem.classList.contains('reverse-shatter-animation')) {
+                            skillsItem.classList.remove('reverse-shatter-animation')
+                        }
+                    }, 1500)
+                }
+            }, 500);
+        }
+    })
+});
 
 
 
@@ -425,31 +380,4 @@ swiperInit(jsWorks);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const preLoader = () => {
-//     const preloader = document.querySelector('.preloader');
-//
-//     window.addEventListener('load', () => {
-//         if (preloader) {
-//             preloader.style.display = 'flex'
-//             setTimeout(() => {
-//                 preloader.style.display = 'none';
-//                 ScrollTrigger.refresh();
-//             }, 1000);
-//         }
-//     });
-// };
-
-// preLoader();
 
